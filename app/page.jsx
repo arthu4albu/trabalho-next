e0a858aaff6906d83b7014602515a8796c85badc
Arthur Albuquerque <lourencoarthur224@gmail.com>
2024-10-07 22:20:24 -0300

    add loggin page

---
+++ app/page.jsx
@@ -0,0 +1,37 @@
+import "./style.css"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <img src="/images/foto.png" alt="Imagem de perfil" />
+
+        <form action="">
+          <div className="user">
+            <label htmlFor="user">Usuário: </label>
+            <input type="text" name="user" placeholder="Digite aqui..." />
+          </div>
+
+          <div className="password">
+            <label htmlFor="password">Senha: </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Digite aqui..."
+            />
+            <small>
+              <Link href="/senha">Esqueceu a senha.</Link>
+            </small>
+          </div>
+
+          <button type="submit">Entrar</button>
+          <small>
+            <Link href="/cadastro">Cadastre-se</Link>
+          </small>
+        </form>
+      </div>
+    </>
+  )
+}
