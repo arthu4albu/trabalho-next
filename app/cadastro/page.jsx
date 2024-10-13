942649d0cac565b91c76a000cdfb21b72ed22e36
Arthur Albuquerque <lourencoarthur224@gmail.com>
2024-10-13 15:26:59 -0300

    Add register page

---
+++ app/cadastro/page.jsx
@@ -0,0 +1,55 @@
+import Link from "next/link"
+import "./style.css"
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <img src="/images/foto.png" alt="Imagem de perfil" />
+
+        <form action="">
+          <div className="input-area">
+            <div className="user input">
+              <label htmlFor="user">Usuário: </label>
+              <input type="text" name="user" placeholder="Digite aqui..." />
+            </div>
+
+            <div className="password input">
+              <label htmlFor="password">Senha: </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Digite aqui..."
+              />
+
+              <ul>
+                <li>A senha deve conter entre 8 a 13 dígitos</li>
+                <li>
+                  A senha deve conter letras masc, letras mins, números e
+                  símbolos
+                </li>
+              </ul>
+            </div>
+
+            <div className="identification-code input">
+              <label htmlFor="user">Código de identificação: </label>
+              <input type="text" name="user" placeholder="Digite aqui..." />
+            </div>
+
+            <div className="confirm-password input">
+              <label htmlFor="user">Confirmar senha: </label>
+              <input type="text" name="user" placeholder="Digite aqui..." />
+            </div>
+          </div>
+
+          <div className="submit-button">
+            <button type="submit">Cadastrar</button>
+            <small>
+              <Link href="/">Fazer login</Link>
+            </small>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import "./style.css"
-import Image from "next/image"
 import Link from "next/link"
 
 export default function Home() {
